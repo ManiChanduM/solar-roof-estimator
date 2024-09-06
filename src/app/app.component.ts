0e4f534135e6304256083d98af09e21b8f9f38c9
ManiChanduM <manichandu.m@gmail.com>
2024-09-07 03:27:10 +0530

    add google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GoogleMap],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'solar-roof-estimator';
+  center!: google.maps.LatLngLiteral | undefined;
+  display!: google.maps.LatLngLiteral | undefined;
+  options: google.maps.MapOptions = {
+    center: { lat: 40, lng: -100 },
+    zoom: 4
+  };
+
+  moveMap(event: google.maps.MapMouseEvent) {
+    this.center = (event.latLng?.toJSON());
+  }
+
+  move(event: google.maps.MapMouseEvent) {
+    this.display = event.latLng?.toJSON();
+  }
 }
