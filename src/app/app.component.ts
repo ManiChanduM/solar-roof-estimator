f89ef0133d8b649a393c3e4ae05b7fb48dadfbac
ManiChanduM <manichandu.m@gmail.com>
2024-09-07 16:13:51 +0530

    add primeng and bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GoogleMap } from '@angular/google-maps';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GoogleMap],
+  imports: [RouterOutlet, GoogleMap, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'solar-roof-estimator';
   center!: google.maps.LatLngLiteral | undefined;
   display!: google.maps.LatLngLiteral | undefined;
@@ -18,11 +20,17 @@ export class AppComponent {
     zoom: 4
   };
 
+  constructor(private primengConfig: PrimeNGConfig) { }
+
   moveMap(event: google.maps.MapMouseEvent) {
     this.center = (event.latLng?.toJSON());
   }
 
   move(event: google.maps.MapMouseEvent) {
     this.display = event.latLng?.toJSON();
+  }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 }
